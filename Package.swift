73c454f5f1697cb1a485e7c992c48d8a66ad7638
Бронников Андрей Александрович <a.bronnikov@2Gis.ru>
2021-01-19 14:58:58 +0700

    xcframework with markers

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
 		.binaryTarget(
 			name: "PlatformSDK",
-			url: "https://maven.2gis.dev/ios-frameworks/feature-SDK-372-ios-library-with-generated-interfaces/Release/PlatformSDK.xcframework.zip",
-			checksum: "92f6e075a0c37cd4bbaf19d2a6da2765362dd2c96fcddf4b0a75fab9c7f5a1b0"
+			url: "https://maven.2gis.dev/ios-frameworks/feature-SDK-382-ios-marker-builder/Release/PlatformSDK.xcframework.zip",
+			checksum: "7f5c70ab95b24dff0c71c528438edb4a59ab6b89ed1720282721accef3bffb33"
 		)
     ]
 )
