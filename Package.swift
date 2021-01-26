299eef4ae56428623899577f3821cce788d53d81
Бронников Андрей Александрович <a.bronnikov@2Gis.ru>
2021-01-26 19:26:18 +0700

    xcframework with source deletion

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
 		.binaryTarget(
 			name: "PlatformSDK",
-			url: "https://maven.2gis.dev/ios-frameworks/feature-SDK-387-map-control/Release/PlatformSDK.xcframework.zip",
-			checksum: "9b120c609bd07e6e3e6c7f51c24ef001140fde0141d2cdf16446e599e8438d3e"
+			url: "https://maven.2gis.dev/ios-frameworks/feature-SDK-%23-route-source-wa/Release/PlatformSDK.xcframework.zip",
+			checksum: "58152509f3ef030b2b916d51c665332da8baf1455cadc5014c6d1f3d367eeed8"
 		)
     ]
 )
