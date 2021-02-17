99fabe3d6bed8cabc85e9c6da256107d8e1ef326
Anatoly Petrov <petrov.an@2Gis.ru>
2021-02-17 14:53:40 +0700

    Propertly update to 0.6.0

--- Package.swift
+++ Package.swift
@@ -9,10 +9,18 @@ let package = Package(
         .library(
             name: "NativeSDK",
             targets: ["PlatformSDK"]),
+        .library(
+            name: "NativeMapSDK",
+            targets: ["PlatformMapSDK"]),
     ],
     targets: [
 		.binaryTarget(
 			name: "PlatformSDK",
+			url: "https://maven.2gis.dev/ios-frameworks/0.6.0/Release/PlatformSDK.xcframework.zip",
+			checksum: "96458f704826a50fdad381b11706d59934da5abccee6fd0fa2654c52ffc75452"
+		),
+		.binaryTarget(
+			name: "PlatformMapSDK",
 			url: "https://maven.2gis.dev/ios-frameworks/0.6.0/Release/PlatformMapSDK.xcframework.zip",
 			checksum: "7e8072f63ec045aaaeb45c2df5813211a85b6e2383bd338e54b6c3eebb539e2f"
 		)
