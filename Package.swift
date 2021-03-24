3625a2808f767136ea6fa69f87b252076586a034
a.maksimovskiy <a.maksimovskiy@2Gis.ru>
2021-03-24 14:10:33 +0300

    Add version 0.9.0

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
 		.binaryTarget(
 			name: "PlatformSDK",
-			url: "https://maven.2gis.dev/ios-frameworks/0.8.0/Release/PlatformSDK.xcframework.zip",
-			checksum: "26b0779b48face64a6f8126cfebfe4528f10b471b8794373e929f6b3e13fde7b"
+			url: "https://maven.2gis.dev/ios-frameworks/0.9.0/Release/PlatformSDK.xcframework.zip",
+			checksum: "89adfeae120d87b73ac2cb76c3318370c0b93b6a207cf238533cf6b5b41f8da5"
 		),
 		.binaryTarget(
 			name: "PlatformMapSDK",
-			url: "https://maven.2gis.dev/ios-frameworks/0.8.0/Release/PlatformMapSDK.xcframework.zip",
-			checksum: "6c44a63f53442ba51ea5cd2a3f614cc68d874db839a803b0324f28345d4b6e8b"
+			url: "https://maven.2gis.dev/ios-frameworks/0.9.0/Release/PlatformMapSDK.xcframework.zip",
+			checksum: "4396e75ec17c6955a2d6df6510d8f7cc40952741a42ef60a6cf15a05e13a1bf4"
 		)
     ]
 )
