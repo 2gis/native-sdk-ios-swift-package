c021b71cd399795443039665609aa65e2286e7a4
buildserver-v4core <uk.github.auth@2gis.ru>
2021-06-07 19:26:53 +0700

    Bump version to 0.16.1

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
 		.binaryTarget(
 			name: "PlatformSDK",
-			url: "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v0.16.1/Release/full-RC-1623058622951.xcframework.zip",
+			url: "https://maven.2gis.dev/sdk-ios-release/0.16.1/Release/PlatformSDK.xcframework.zip",
 			checksum: "aa6fc982ea8098c1437a5e010718730370beb143dbb3c0f64ad9678f22102c34"
 		),
 		.binaryTarget(
 			name: "PlatformMapSDK",
-			url: "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v0.16.1/Release/map-RC-1623058622951.xcframework.zip",
+			url: "https://maven.2gis.dev/sdk-ios-release/0.16.1/Release/PlatformMapSDK.xcframework.zip",
 			checksum: "319afff908d1c90edef3fa8014fca37ee6ff9f2bdd4809c2c7eb8a3bc0e562c5"
 		)
     ]
