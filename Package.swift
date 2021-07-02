4a5cbdbfec2489609d7489974fa0e079c9c736e8
buildserver-v4core <uk.github.auth@2gis.ru>
2021-07-02 16:11:30 +0700

    Bump version to 1.0.0-rc.7

--- Package.swift
+++ Package.swift
@@ -1,28 +1,19 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "NativeSDK",
+    name: "DGisMobileSDK",
     products: [
         .library(
-            name: "NativeSDK",
-            targets: ["PlatformSDK"]),
-        .library(
-            name: "NativeMapSDK",
-            targets: ["PlatformMapSDK"]),
+            name: "MapSDK",
+            targets: ["DGis"]),
     ],
     targets: [
 		.binaryTarget(
-			name: "PlatformSDK",
-			url: "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v1.0.0/Release/full-RC-1625158954969.xcframework.zip",
-			checksum: "e90c84912ce258307c3772b25ad2e75303f26f44430d0ba4ceecd16948358b43"
-		),
-		.binaryTarget(
-			name: "PlatformMapSDK",
-			url: "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v1.0.0/Release/map-RC-1625158954969.xcframework.zip",
-			checksum: "e109db7b1b4a05104ef037079e1d721912ce972c40b831570f726e813b8ccd2c"
+			name: "DGis",
+			url: "",
+			checksum: ""
 		)
     ]
 )
